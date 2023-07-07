474185bbb05be9c16c5a2a33687edc76f2e085dd
Meir benin <meirbenin@Meirs-MacBook-Air.local>
2023-07-07 16:28:31 +0300

    set url by user

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
+
 import subprocess
+
 
 app = Flask(__name__)
 
 @app.route('/')
-def stream():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    url = 'https://kan11.media.kan.org.il/hls/live/2024514/2024514/source1_4k/chunklist.m3u8'
-
-    ffmpeg_process = subprocess.Popen(['ffmpeg', '-i', url, '-f', 'mp3', 'pipe:1'], stdout=subprocess.PIPE)
-
+def convert_stream_to_mp3():
+    """convert m3u8 stream to mp3"""
+    url = request.args.get('url','')
     def generate():
-        # Read the output from FFmpeg process and yield it to the client
-        while True:
-            data = ffmpeg_process.stdout.read(1024)
-            if not data:
-                print('breaking')
-                break
-            yield data
+       print(url)
+       with subprocess.Popen(['ffmpeg', '-i', url, '-f', 'mp3', '-'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT) as process:
+           while True:
+                data = process.stdout.read(1024)
+                if not data:
+                    break
+                yield data
 
-    # Return the generated stream as a Flask response
-    return Response(generate(), mimetype='audio/mp3')
+    response = Response(generate(), mimetype='audio/mp3')
+    response.headers.set('Cache-Control', 'no-cache')
+    response.headers.set('Connection', 'keep-alive')
+    return response
+
 
 if __name__ == '__main__':
     app.run("0.0.0.0",port=5554)
